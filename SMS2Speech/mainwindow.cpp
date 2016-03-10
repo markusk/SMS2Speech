e02aaa226c1e042c48d2ab062caa4da4d06402f0
Markus Knapp <github@direcs.de>
2016-03-10 23:26:49 +0100

    Updated textEdit field. Added appendLog, on_pushButtonQuit_clicked and speak Signal. Included QDateTime.

--- SMS2Speech/mainwindow.cpp
+++ SMS2Speech/mainwindow.cpp
@@ -12,3 +12,38 @@ MainWindow::~MainWindow()
 {
 	delete ui;
 }
+
+void MainWindow::on_pushButtonQuit_clicked()
+{
+	close();
+}
+
+void MainWindow::appendLog(QString text, bool CR, bool sayIt, bool addTimestamp)
+{
+	if (addTimestamp == true)
+	{
+		// get the current date and time for a timestimp in the log
+		now = QDateTime::currentDateTime();
+
+		// prepend timestamp
+		text = QString("%1:%2:%3 %4").arg(now.toString("hh")).arg(now.toString("mm")).arg(now.toString("ss")).arg(text);
+	}
+
+	// insert the new text in the GUI
+	ui->textEditOutput->insertHtml(text);
+
+	if (CR == true) // default!
+	{
+		ui->textEditOutput->insertHtml("<br>");
+	}
+
+	// Ensures that the cursor is visible by scrolling the text edit if necessary.
+	ui->textEditOutput->ensureCursorVisible();
+
+
+	// say the message via Linux espeak
+	if (sayIt == true)
+	{
+		emit speak(text);
+	}
+}
--- SMS2Speech/mainwindow.h
+++ SMS2Speech/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDateTime>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,31 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+public slots:
+	/**
+	Appends text to the main log in the main window.
+	@param text is the text to be displayed.
+	@param CR adds a carriage return (CR) to the text, if true (default). This parameter is optional.
+	@param sayIt If true, the text is also spoken (default=false). This parameter is optional.
+	@param addTimestamp If true, the a timestamp is added in front of the text. This parameter is optional.
+	 */
+	void appendLog(QString text, bool CR=true, bool sayIt=false, bool addTimestamp=true);
+
+protected slots:
+	/**
+	Action if a menu item or a toolbar button is clicked
+	*/
+	void on_pushButtonQuit_clicked();
+
+signals:
+	/**
+	Emits a speak signal. This signal is sent to the speakThread.
+	*/
+	void speak(QString text);
+
 private:
 	Ui::MainWindow *ui;
+	QDateTime now; /// this is for the timestamp in the logs in the gui
 };
 
 #endif // MAINWINDOW_H
